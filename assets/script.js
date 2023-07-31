04a496cd137d4a9529689caed5ba601d2f399f85
jorjik81 <jorjik81@gmail.com>
2023-07-31 17:23:39 +0400

    JS first class

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for (var x = 0; x < topics.length; x++) {
+var shapes = ['html', 'CSS', 'Git', "JavaScript"];
+for(var x = 0; x < shapes.length; x++){
+  console.log(shapes[x]);
+}
+
+
+
+/*var topic = "HTML";
+if (topic === 'html')
+{console.log ("Lets Study HTML");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+  /*var topics = "html, CSS, Git, JavaScript";
+console.log(topics);
+/*for (var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
    }
 /*
